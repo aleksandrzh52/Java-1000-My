567c33b8cf4104f4d8373be52ab1ac71173ca77a
aleksandrzh52 <96547517+aleksandrzh52@users.noreply.github.com>
2023-01-15 00:18:55 +0300

    NotDeafPhone

--- no1/NotDeafPhone.java
+++ no1/NotDeafPhone.java
@@ -1,4 +1,23 @@
 package no1;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class NotDeafPhone {
+    public static void main(String[] args) throws IOException {
+        FileReader fileReader = new FileReader("./no1/input.txt");
+        Scanner scanner = new Scanner(fileReader);
+        String inScanner = scanner.nextLine();
+
+        FileWriter fileWriter = new FileWriter("./no1/output.txt");
+        fileWriter.write(inScanner);
+        System.out.println(inScanner);
+        fileWriter.close();
+
+
+
+    }
 }
